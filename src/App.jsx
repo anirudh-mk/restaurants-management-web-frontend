d2b2dc7e5ca30a7d16d25422c565fffb7ab6b34a
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-03-14 21:56:50 +0530

    feat(production error fixed)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-import Headder from "./components/widgets/Headder/Headder";
 import RestaurantScreen from "./components/screens/RestaurantScreen/RestaurantScreen";
 
 function App() {
