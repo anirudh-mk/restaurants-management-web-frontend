06236576e7143ef61a36b5abb11550e7cf39a893
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-03-15 00:07:32 +0530

    feat(database integration)

--- src/components/widgets/ImageCard/ImageCard.jsx
+++ src/components/widgets/ImageCard/ImageCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './ImageCard.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
-function ImageCard({ src, alt, name, rating, size }) {
+function ImageCard({ src, alt, name, rating, size, price }) {
     return (
         <div className='image-card' style={size == 'lg' ? { padding: 0, marginTop: 16, marginBottom: 16 } : { padding: 5 }}>
             <img
@@ -12,10 +12,13 @@ function ImageCard({ src, alt, name, rating, size }) {
                 style={size === 'lg' ? { width: '100%' } : null}
             />
             <div className='image-card--headdings' style={size == 'lg' ? { paddingInline: 16 } : {}} >
-                <p>Burger</p>
+                <p
+                    className='image-card--title'
+                    style={size !== 'lg' ? { whiteSpace: 'nowrap', width: 50, overflow: 'hidden', textOverflow: 'ellipsis' } : {}}
+                >{name}</p>
                 <div className='image-card--rating'>
                     <FontAwesomeIcon icon={faStar} size='md' color='#FFD600' />
-                    <p>4.2</p>
+                    <p>{rating}</p>
                 </div>
             </div>
             <div className='image-card--sub-headding' style={size === 'lg' ? { paddingBottom: 16, paddingInline: 16, justifyContent: 'space-between' } : {}}>
@@ -23,7 +26,7 @@ function ImageCard({ src, alt, name, rating, size }) {
                     <div className='circle' />
                     <p>veg</p>
                 </div>}
-                <p className='image-card-amount' style={size !== 'lg' ? { justifyContent: 'end', width: '100%' } : {}}>₹ {120}</p>
+                <p className='image-card-amount' style={size !== 'lg' ? { justifyContent: 'end', width: '100%' } : {}}>₹ {price}</p>
             </div>
         </div >
     )
